907843e31c985602c80e00173e57b723c0fc400b
Chavisky D <auukunn.bkk@gmail.com>
2026-04-04 09:57:18 +0700

    changing the recruitment

--- sc-pharm-form/src/config/branches.js
+++ sc-pharm-form/src/config/branches.js
@@ -11,8 +11,8 @@ const BRANCH_BY_ID = BRANCHES.reduce((acc, branch) => {
 }, {});
 
 // Edit these arrays to change which branches are recruiting this round.
-export const pharmacistEnabled = ["005", "003"];
-export const salesEnabled = ["003"];
+export const pharmacistEnabled = ["005"];
+export const salesEnabled = ["005"];
 
 export function getEnabledBranchIds(role) {
   if (role === "pharmacist") return [...pharmacistEnabled];
